e0ff7ab97556fc4a3161aa7241c0ca282cb912e4
Arun Kushwah <arunkushwah592005@gmail.com>
2025-08-09 09:15:41 +0530

    Add files via upload

---
+++ Minimum_Absolute_Difference_in_BST_530.java
@@ -0,0 +1,26 @@
+package LEETCODE;
+
+public class Minimum_Absolute_Difference_in_BST_530 {
+
+	
+	public class TreeNode {
+		 *     int val;
+		 *     TreeNode left;
+		 *     TreeNode right;
+		 *     TreeNode() {}
+		 *     TreeNode(int val) { this.val = val; }
+		 *     TreeNode(int val, TreeNode left, TreeNode right) {
+		 *         this.val = val;
+		 *         this.left = left;
+		 *         this.right = right;
+		 *     }
+		 * }
+		 */
+		class Solution {
+		    public int getMinimumDifference(TreeNode root) {
+		        
+		    }
+		}
+	
+	
+}
